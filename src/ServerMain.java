51cdd7ba663dcfc1f8d51bf1fc32fe0e5f7eb08e
MOHAMMADALSIFAN <alsifan01@gmail.com>
2024-10-07 12:40:25 +0800

    Third commit

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,29 +1,9 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-
 public class ServerMain {
 
-    
-    public static void main(String[] args) throws IOException {
-        int port = 5000;
-        if (args.length > 0) 
-        {
-            port = Integer.parseInt(args[0]);
-            
-            System.out.printf(">> Listening on port %d " , port);
-
-        } 
-
-         ServerSocket server = new ServerSocket(port);
-
-         //create the server
-
-         System.out.println("\nWaiting for connection ");
+    public static void main(String[] args) throws IO {
 
 
 
-
-
-
-        }
     }
+    
+}
